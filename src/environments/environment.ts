7d45d0be1edaee86fe21fad9c1b8895bff5ce08f
DefMaks <defmaks@gmail.com>
2023-07-01 15:30:04 +0100

    01/07/2023 15:30

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appName: 'EazzyCare',
+  appLogo: 'assets/eazzyCare-Icon.png',
+  appLogo_ALT: 'assets/eazzyCare-Icon.svg',
+  // SUPABASE
+  supabaseUrl: 'https://cuxxtjucymaigupbsavy.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1eHh0anVjeW1haWd1cGJzYXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA2MTc2NDYsImV4cCI6MTk5NjE5MzY0Nn0.ROf1_rGHmAFSv2aaVlsf0kPy5Rs_hLWoa4oVgB9aKBA',
+  supabaseKeyService: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1eHh0anVjeW1haWd1cGJzYXZ5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MDYxNzY0NiwiZXhwIjoxOTk2MTkzNjQ2fQ.Hsm4R4m93zHFUfVLqa7blfWqSKox7cV1Y9oPBFrjslk',
+
 };
 
 /*
